42a856e8fc0d5174417174fbff262eda94f7d3b1
KevinRabbe <rabbekevin@icloud.com>
2026-08-06 18:06:20 +0200

    Expose self-improvement policy and evaluation APIs

--- src/plural_cognition/self_improvement/__init__.py
+++ src/plural_cognition/self_improvement/__init__.py
@@ -8,6 +8,11 @@ from .candidate_pool import (
     GenerationSource,
     build_frozen_candidate_pool,
 )
+from .evaluation import (
+    PolicyEvaluation,
+    PolicyTaskScore,
+    evaluate_policy_on_split,
+)
 from .genome import (
     BehaviorDescriptor,
     IMMUTABLE_PARENT_GENOME,
@@ -20,6 +25,12 @@ from .mutation import (
     apply_mutation,
     propose_neighbor_mutations,
 )
+from .policy import (
+    PolicyExecution,
+    PolicyResourceTrace,
+    ReasoningBudget,
+    execute_reasoning_policy,
+)
 
 __all__ = [
     "BehaviorDescriptor",
@@ -30,10 +41,17 @@ __all__ = [
     "GenerationSource",
     "IMMUTABLE_PARENT_GENOME",
     "MutationRecord",
+    "PolicyEvaluation",
+    "PolicyExecution",
     "PolicyMode",
+    "PolicyResourceTrace",
+    "PolicyTaskScore",
     "ProposedMutation",
+    "ReasoningBudget",
     "ReasoningPolicyGenome",
     "apply_mutation",
     "build_frozen_candidate_pool",
+    "evaluate_policy_on_split",
+    "execute_reasoning_policy",
     "propose_neighbor_mutations",
 ]
