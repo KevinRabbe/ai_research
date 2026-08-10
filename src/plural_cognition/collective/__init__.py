60527c71c2470cc8e5634f73f7f51a1c0129520f
KevinRabbe <rabbekevin@icloud.com>
2026-08-11 00:16:02 +0200

    Export collective uplift metrics

--- src/plural_cognition/collective/__init__.py
+++ src/plural_cognition/collective/__init__.py
@@ -12,21 +12,43 @@ from .artifacts import (
     TaskIdentity,
     sha256_content,
 )
+from .metrics import (
+    CollectiveSummary,
+    ErrorCorrelation,
+    FourMindCredit,
+    OutcomeTable,
+    StageDelta,
+    compare_stages,
+    error_correlation_matrix,
+    evaluate_four_mind_credit,
+    pairwise_error_correlation,
+    summarize_collective,
+)
 from .mind import MindBackend, MindIdentity, MindInvocationResult, MindRequest
 
 __all__ = [
     "ARTIFACT_SCHEMA",
     "EVALUATION_SCHEMA",
     "CollectiveStage",
+    "CollectiveSummary",
+    "ErrorCorrelation",
     "EvaluationRecord",
     "EvaluationVisibility",
+    "FourMindCredit",
     "MetricValue",
     "MindBackend",
     "MindIdentity",
     "MindInvocationResult",
     "MindRequest",
+    "OutcomeTable",
     "ResourceUsage",
     "StageArtifact",
+    "StageDelta",
     "TaskIdentity",
+    "compare_stages",
+    "error_correlation_matrix",
+    "evaluate_four_mind_credit",
+    "pairwise_error_correlation",
     "sha256_content",
+    "summarize_collective",
 ]
